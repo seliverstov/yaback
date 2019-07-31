d9d7b437ee417cf3e73758d784c5df3cabbca44d
Aleksandr Seliverstov <seliverstov.a@gmail.com>
2019-07-31 18:18:02 +0300

    feat: add test for each fields on import

--- tests/imports.py
+++ tests/imports.py
@@ -47,3 +47,117 @@ def test_import_mutual_relatives():
     assert "data" in result
     assert "import_id" in result['data']
 
+
+def __test_import_int_field(field):
+    server_api = get_server_api()
+    citizen = get_random_citizen(relatives=False)
+    citizen[field] = None
+    data = {
+        'citizens': [citizen]
+    }
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "abc"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "1"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 201
+    assert "data" in result
+    assert "import_id" in result['data']
+
+
+def __test_import_str_field(field):
+    server_api = get_server_api()
+    citizen = get_random_citizen(relatives=False)
+    citizen[field] = None
+    data = {
+        'citizens': [citizen]
+    }
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "a"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 201
+    assert "data" in result
+    assert "import_id" in result['data']
+
+
+def __test_import_date_field(field):
+    server_api = get_server_api()
+    citizen = get_random_citizen(relatives=False)
+    citizen[field] = None
+    data = {
+        'citizens': [citizen]
+    }
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "abc"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "40.14.1000"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = 100000
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 422
+
+    citizen[field] = "10.10.2019"
+    r = requests.post(f"{server_api}/imports", json=data)
+    result = r.json()
+    print(f"RESPONSE JSON: {result}")
+    assert r.status_code == 201
+    assert "data" in result
+    assert "import_id" in result['data']
+
+
+def test_import_citizen_id():
+    __test_import_int_field('citizen_id')
+
+
+def test_import_appartement():
+    __test_import_int_field('appartement')
+
+
+def test_import_town():
+    __test_import_str_field('town')
+
+
+def test_import_street():
+    __test_import_str_field('street')
+
+
+def test_import_name():
+    __test_import_str_field('name')
+
+
+def test_import_gender():
+    __test_import_str_field('gender')
+
+
+def test_import_birth_date():
+    __test_import_date_field('birth_date')
